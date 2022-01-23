12869229de5dfad0137c48a512446ca9093b9d6d
Adrian Ang <adriancbang@gmail.com>
2022-01-22 16:16:37 -0800

    Add simple UI to game and refactor helper functions

--- foundations/project-rock-paper-scissors/script.js
+++ foundations/project-rock-paper-scissors/script.js
@@ -1,74 +1,84 @@
+game();
+
 function game() {
+  const playerOptions = document.querySelectorAll('#player-options button');
+  const moveRecapSummary = document.querySelector('#move-recaps');
+  const moveRecap = document.createElement('p');
+  const playerScoreboard = document.querySelector('#player-score');
+  const cpuScoreboard = document.querySelector('#cpu-score');
+  const newGameButton = document.querySelector('#new-game');  
   let playerScore = 0;
-  let computerScore = 0;
+  let cpuScore = 0;
 
-  console.log(`
-    ==============================================
-    NOW STARTING: Rock, Paper, Scissors Tournament
-    ==============================================
-    - Best out of 5 rounds.
-    - Draws and incorrect moves do not affect
-      scoring, but still ends the round.
-    - Tournament ends early if you make no move.
-    - Good luck! :)
-  `);
+  moveRecap.innerHTML = `Make a move to start a new game. <br />
+    First to five points wins!`;
+  moveRecapSummary.appendChild(moveRecap);
 
-  for (let i = 0; i < 5; i++) {
-    getScoreboard(playerScore, computerScore);
-    console.log(`ROUND ${i + 1} of 5:`);
-    let playerMove = prompt('Pick one: rock, paper, or scissors?');
-    let roundResult = playRound(playerMove, computerPlay());
+  playerOptions.forEach((button) => {
+    button.addEventListener('click', () => {
+      const playerMove = titleCaseMove(button.getAttribute('data-key'));
+      const computerMove = computerPlay();
+      const roundResult = playRound(playerMove, computerMove);
+      updateScoreboard(playerScore, cpuScore, playerScoreboard, cpuScoreboard);
 
-    if (roundResult === 'Win') {
-      playerScore++;
-    } else if (roundResult === 'Loss') {
-      computerScore++;
-    } else if (roundResult === null) {
-      console.log('No move made.\nTournament ended early.');
-      return null;
+      if (roundResult === 'Win') {
+        playerScore++;
+        updateScoreboard(playerScore, cpuScore, playerScoreboard, cpuScoreboard);
+        moveRecap.innerHTML = `You win! +1 to you!<br />
+          ${playerMove} beats ${computerMove}!`;
+      } else if (roundResult === 'Loss') {
+        cpuScore++;
+        updateScoreboard(playerScore, cpuScore, playerScoreboard, cpuScoreboard);
+        moveRecap.innerHTML = `You lose! +1 for the CPU :(<br />
+          ${computerMove} beats ${playerMove}!`;
+      } else {
+        moveRecap.innerHTML = `It's a tie, you both picked ${playerMove}.<br />
+          No points awarded.`;
+      }
+
+      if (playerScore === 5) {
+        moveRecap.innerHTML = `Congratulations -- you won the tournament!<br />
+          Click on New Game to play again!`;
+        toggleDisabledButtons(playerOptions);
+      } else if (cpuScore === 5) {
+        moveRecap.innerHTML = `You lost -- better luck next time :(<br />
+          Click on New Game to play again!`;
+        toggleDisabledButtons(playerOptions);
+      }
+    });
+  });
+
+  newGameButton.addEventListener('click', () => {
+    let resetReason;
+    if (playerScore === 5 || cpuScore === 5) {
+      resetReason = 'New game';
+      toggleDisabledButtons(playerOptions);
+    } else if (playerScore < 5 && cpuScore < 5) {
+      resetReason = 'Game reset by player';
     }
-  }
 
-  getScoreboard(playerScore, computerScore);
-  getTournamentWinner(playerScore, computerScore);
+    moveRecap.innerHTML = `${resetReason} -- scores set back to 0.<br />
+      Good luck this time!`;
+    playerScore = 0;
+    cpuScore = 0;
+    updateScoreboard(playerScore, cpuScore, playerScoreboard, cpuScoreboard);
+  });
 }
 
-function getScoreboard(playerScore, computerScore) {
-  let scoreboard = `
-    ==========================
-    ======= SCOREBOARD =======
-    ==========================
-    ===     ==========     ===
-    === YOU ========== CPU ===
-    ===  ${playerScore}  ==========  ${computerScore}  ===
-    ===     ==========     ===
-    ==========================
-  `;
-
-  console.log(scoreboard);
+function titleCaseMove(move) {
+  let titleCasedMove = `${move[0].toUpperCase() + move.slice(1).toLowerCase()}`;
+  return titleCasedMove;
 }
 
-function getTournamentWinner(finalPlayerScore, finalComputerScore) {
-  let winner;
-  let winnerAnnouncement;
+function toggleDisabledButtons(buttons) {
+  buttons.forEach((button) => {
+    button.toggleAttribute('disabled');
+  });
+}
 
-  if (finalPlayerScore > finalComputerScore) {
-    winner = 'You';
-    winnerAnnouncement = 'Congratulations -- you won the tournament!';
-  } else if (finalPlayerScore < finalComputerScore) {
-    winner = 'Computer';
-    winnerAnnouncement = 'You lost -- better luck next time :(';
-  } else {
-    winner = 'You && Computer';
-    winnerAnnouncement = 'It\'s a tie -- play again?';
-  }
-
-  console.log(`
-    ==========================================
-    FINAL WINNER: ${winner}
-    ==========================================
-    ${winnerAnnouncement}
-  `);
+function updateScoreboard(playerScore, cpuScore, playerScoreboard, cpuScoreboard) {
+  playerScoreboard.textContent = `You: ${playerScore}`;
+  cpuScoreboard.textContent = `CPU: ${cpuScore}`;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -76,27 +86,21 @@ function playRound(playerSelection, computerSelection) {
     return null;
   }
 
-  playerSelection = `${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()}`;
-
   if (playerSelection === computerSelection) {
-    console.log(`It's a tie -- you both picked ${playerSelection}! No points awarded.`);
     return 'Draw';
   } else if (
     playerSelection === 'Rock' && computerSelection === 'Paper' ||
     playerSelection === 'Paper' && computerSelection === 'Scissors' ||
     playerSelection === 'Scissors' && computerSelection === 'Rock'
   ) {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
     return 'Loss';
   } else if (
     playerSelection === 'Rock' && computerSelection === 'Scissors' ||
     playerSelection === 'Paper' && computerSelection === 'Rock' ||
     playerSelection === 'Scissors' && computerSelection === 'Paper'
   ) {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
     return 'Win';
   } else {
-    console.log('Something went wrong -- make sure you spelled your move correctly...');
     return 'Error';
   }
 }
