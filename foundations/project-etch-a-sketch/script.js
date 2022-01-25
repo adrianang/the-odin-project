1aa043e1ca8cc9d4e192108086f538f1c9eecc4d
Adrian Ang <adriancbang@gmail.com>
2022-01-25 02:23:57 -0800

    Edit functionality of clear button to also ask user for the size of the new grid, refactor canvas initialization

--- foundations/project-etch-a-sketch/script.js
+++ foundations/project-etch-a-sketch/script.js
@@ -4,6 +4,24 @@ function initializeCanvas(size) {
   const clearButton = document.querySelector('#clear-btn');
   const canvas = document.querySelector('#canvas');
 
+  newCanvas(canvas, size);
+
+  clearButton.addEventListener('click', () => {
+    while (canvas.firstChild) {
+      canvas.firstChild.remove();
+    }
+
+    let newSize = Number(prompt('New size of grid (1—100)?'));
+    if (newSize > 0 && newSize < 101) {
+      newCanvas(canvas, newSize);
+    } else {
+      alert('Invalid input -- defaulted to a 4x4 grid.');
+      newCanvas(canvas, 4);
+    }
+  });
+}
+
+function newCanvas(canvas, size) {
   canvas.setAttribute('style',
     `grid-template-rows: repeat(${size}, 1fr);
      grid-template-columns: repeat(${size}, 1fr);`
@@ -17,12 +35,4 @@ function initializeCanvas(size) {
 
     canvas.appendChild(canvasPixel);
   }
-
-  clearButton.addEventListener('click', () => {
-    for (let i = 0; i < (4 * 4); i++) {
-      canvas.removeChild(canvas.childNodes[0]);
-    }
-
-    initializeCanvas(4);
-  });
 }
