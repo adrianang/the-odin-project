093a007dc62767152a66e664b5ae4cabd96e94c4
Adrian Ang <adriancbang@gmail.com>
2022-02-01 04:57:06 -0800

    Add number and operator buttons as initial UI, basic calculation for equations with two numbers

--- foundations/project-calculator/script.js
+++ foundations/project-calculator/script.js
@@ -1,3 +1,74 @@
+initializeCalculator();
+
+function initializeCalculator() {
+  const display = document.querySelector('#display');
+  const numbers = document.querySelectorAll('.number-val-btn');
+  const operators = document.querySelectorAll('.operator-action-btn');
+  const equalsButton = document.querySelector('#equals-btn');
+  const clearButton = document.querySelector('#clear-btn');
+  let displayValue = '0';
+  let value1 = '';
+  let value2 = '';
+  let valueInConstruction = '1';
+  let operatorToApply = '';
+  let result;
+  display.textContent = displayValue;
+
+  numbers.forEach((numberBtn) => {
+    numberBtn.addEventListener('click', () => {
+      if (valueInConstruction === '1') {
+        value1 += numberBtn.getAttribute('data-value');
+        displayValue = value1;
+      } else if (valueInConstruction === '2') {
+        value2 += numberBtn.getAttribute('data-value');
+        displayValue = value2;
+      }
+
+      updateDisplayValue(display, displayValue);
+    });
+  });
+
+  operators.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', () => {
+      operatorToApply = operatorBtn.getAttribute('data-action');
+      displayValue += operatorBtn.textContent;
+      updateDisplayValue(display, displayValue);
+      
+      if (valueInConstruction === '1') {
+        valueInConstruction = '2';
+      } else if (valueInConstruction === '2') {
+        valueInConstruction = '1';
+      }
+    });
+  });
+
+  equalsButton.addEventListener('click',  () => {
+    result = operate(operatorToApply, Number(value1), Number(value2));
+    displayValue = result;
+    updateDisplayValue(display, displayValue);
+
+    value1 = '';
+    value2 = '';
+    displayValue = '0';
+    valueInConstruction = '1';
+    operatorToApply = '';
+  });
+
+  clearButton.addEventListener('click', () => {
+    value1 = '';
+    value2 = '';
+    displayValue = '0';
+    valueInConstruction = '1';
+    operatorToApply = '';
+    result = '';
+    updateDisplayValue(display, displayValue);
+  });
+}
+
+function updateDisplayValue(display, displayValue) {
+  display.textContent = displayValue;
+}
+
 function operate(operator, num1, num2) {
   switch (operator) {
     case 'add':
